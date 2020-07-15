1d94b597cdae85c1caab4b3a6871b15f21b5cae6
giwi97 <giwanthawijekoon@gmail.com>
2020-07-15 17:59:46 +0530

    Minor changes

--- src/cityBank/mainmenu.java
+++ src/cityBank/mainmenu.java
@@ -65,6 +65,11 @@ public class mainmenu extends javax.swing.JFrame {
         jMenu1.add(jMenuItem1);
 
         jMenuItem2.setText("Account");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
@@ -111,7 +116,21 @@ public class mainmenu extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
+
+        Customer cus = new Customer();
+        jDesktopPane1.add(cus);
+        cus.setVisible(true);
+
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+
+        Account acc = new Account();
+        jDesktopPane1.add(acc);
+        acc.setVisible(true);
+
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     /**
      * @param args the command line arguments
